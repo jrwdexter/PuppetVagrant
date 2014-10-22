1da03b4b0f936fbbd4e01b6a7940b21a53ce8cb8
mandest <mandest@gmail.com>
2014-10-22 00:12:55 -0500

    Initial commit with RVM(Ruby1.9), Node and Java.

---
+++ puppet/manifests/default.pp
@@ -0,0 +1,72 @@
+#######################
+# RVM / Ruby and Gems #
+#######################
+
+class ruby_profile (
+    $ruby_version = "ruby-1.9",
+    $system_users = [],
+    $gemset_name = "ruby-1.9@vagrunt",
+    $gems = []
+) {
+    include rvm
+
+    # Ruby
+    rvm_system_ruby {
+        "$ruby_version":
+            ensure => 'present',
+            default_use => true;
+    }
+
+    # System Ruby User
+    each($system_users) |$user| {
+        rvm::system_user {
+            $user:
+        }
+    }
+
+    # Gemset
+    rvm_gemset {
+        "$gemset_name":
+            ensure => present,
+            require => Rvm_system_ruby[$ruby_version]
+    }
+
+    # Gems
+    each($gems) |$package,$version| {
+        rvm_gem {
+            "$gemset_name/$package":
+                ensure => $version,
+                require => Rvm_gemset[$gemset_name];
+        }
+    }
+}
+
+#######################
+# NodeJS and Packages #
+#######################
+
+class nodejs_profile (
+    $version = 'present',
+    $manage_repo = true,
+    $npms = []
+) {
+    class {'nodejs':
+        manage_repo => $manage_repo,
+        version => $version
+    }
+
+    each($npms) |$package,$version| {
+        package { $package:
+            ensure => present,
+            provider => 'npm',
+            require  => Class['nodejs']
+        }
+    }
+}
+
+#######################
+###### EXECUTION ######
+#######################
+include ruby_profile
+include nodejs_profile
+include java
